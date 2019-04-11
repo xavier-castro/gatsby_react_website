0f96fed163924c04317f6715a9ca094318b3d957
xavier-castro <xavier_castro@icloud.com>
2019-04-10 21:39:23 -0500

    Add images and Hero styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,18 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Header 1</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab possimus molestias, saepe blanditiis cupiditate ullam veniam vero magni velit ea reiciendis voluptate veritatis non molestiae voluptatum rem consequuntur suscipit unde.</p>
+        <Link to="/video">Watch the Video</Link>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
