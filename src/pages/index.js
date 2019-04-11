67dc5d3ac2d7ff972f07eb7e24d386198450b5a7
xavier-castro <xavier_castro@icloud.com>
2019-04-11 00:02:15 -0500

    Add JSON with cell mapping

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,34 @@
 import React from "react"
+import styled from "styled-components"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Header from "../components/header"
 import Card from "../components/Card"
 import Section from "../components/Section"
+import staticdata from "../../staticdata.json"
+import Cell from "../components/Cell";
+
+const SectionCaption = styled.p`
+  font-weight: 600;
+  font-size: 18px;
+  text-transform: uppercase;
+  color: #94a4ba;
+  text-align: center;
+`
+
+const SectionCellGroup = styled.div`
+  max-width: 800px;
+  margin: 0 auto 100px;
+  padding: 0 20px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 20px;
+
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(1, fr);
+  }
+`
 
 const IndexPage = () => (
   <Layout>
@@ -107,6 +131,14 @@ const IndexPage = () => (
       title="React for Designers"
       text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify."
     />
+
+  <SectionCaption>12 sections - 6 hours</SectionCaption>
+  <SectionCellGroup>
+    {staticdata.cells.map(cell => (
+      <Cell title={cell.title} image={cell.image} />
+    ))}
+  </SectionCellGroup>
+
   </Layout>
 )
 
