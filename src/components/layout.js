e742d1f0c97c673f2628f7adb8abe776aff4bd46
xavier-castro <xavier_castro@icloud.com>
2019-04-10 22:41:36 -0500

    Add animations and wallpapers

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,32 +14,22 @@ import "./layout.css"
 
 const Layout = ({ children }) => (
   <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
+    // query={graphql`
+    //   query SiteTitleQuery {
+    //     site {
+    //       siteMetadata {
+    //         title
+    //         description
+    //         author
+    //       }
+    //     }
+    //   }
+    // `}
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <div>
           <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
         </div>
       </>
     )}
